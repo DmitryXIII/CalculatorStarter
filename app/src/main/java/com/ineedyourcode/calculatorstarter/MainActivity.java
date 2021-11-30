7bb04b4305be12b902b708e19cba7787710a3d38
Dmitry Gusev <dmtrxiii@gmail.com>
2021-11-30 11:12:39 +0300

    calculator launch released

--- app/src/main/java/com/ineedyourcode/calculatorstarter/MainActivity.java
+++ app/src/main/java/com/ineedyourcode/calculatorstarter/MainActivity.java
@@ -2,6 +2,8 @@ package com.ineedyourcode.calculatorstarter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_launcher).setOnClickListener(v -> {
+            Uri calculatorUri = Uri.parse("calculator://launch");
+
+            startActivity(Intent.createChooser(new Intent(Intent.ACTION_VIEW, calculatorUri), ""));
+        });
     }
 }
