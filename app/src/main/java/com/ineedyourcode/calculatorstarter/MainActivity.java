fd435aafb757a93265f6cbb6225374155f2ffc27
Dmitry Gusev <dmtrxiii@gmail.com>
2021-11-30 11:02:50 +0300

    Initial commit

---
+++ app/src/main/java/com/ineedyourcode/calculatorstarter/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ineedyourcode.calculatorstarter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
